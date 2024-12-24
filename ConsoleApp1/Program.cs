8577506371f239b7f348de2222e128214b911b05
jendos2006 <mikro1108@gmail.com>
2024-12-24 10:57:22 +0300

    Add files via upload

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using HobbyLibrary;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Hobby> hobbies = new List<Hobby>();
+            hobbies.Add(new Hobby("футбол", "забивать мяч в ворота"));
+            hobbies.Add(new Hobby("книги", "читать разные книги"));
+            hobbies.Add(new Hobby("воллейбол", "бить мяч рукой"));
+
+            Person person = new Person("Иванов Евгений", "Троицкая 11", hobbies);
+
+            person.ShowAll();
+            Console.ReadLine();
+        }
+    }
+}
